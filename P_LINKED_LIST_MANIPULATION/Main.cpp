22ba4028b0b19776a7a341821943db77e8fb9485
Chu Van Son <cvson1126034@gmail.com>
2024-11-23 22:36:08 +0700

    P_LINKED_LIST_MANIPULATION

--- P_LINKED_LIST_MANIPULATION/Main.cpp
+++ P_LINKED_LIST_MANIPULATION/Main.cpp
@@ -1,8 +1,199 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct node {
+    int data;
+    struct node * next;
+};
+
+bool existNode(struct node* head, int k){
+    struct node* temp = head;
+    while(temp != NULL){
+        if(temp->data == k){
+            return true;
+        }
+        temp = temp -> next;
+    }
+
+    return false;
+}
+
+struct node* createNode(int k){
+    struct node* newNode = new struct node();
+    newNode->data = k;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addFirst(struct node** head, int k){
+    struct node* temp = *head;
+    if(existNode(temp, k) == false){
+        struct node* newNode = createNode(k);
+        newNode->next = (*head);
+        (*head) = newNode;
+        return;
+    }
+}
+
+void addLast(struct node** head, int k){
+    struct node* temp = *head;
+    if(existNode(temp, k) == false){
+        bool exist = false;
+        struct node* temp = (*head);
+        if(temp == NULL){
+            (*head) = createNode(k);
+            return;
+        }
+
+        else{
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = createNode(k);
+            return;
+        }
+    }
+}
+
+void addBefore(struct node** head, int u, int v){
+    struct node* temp = *head;
+    if(existNode(temp, u) == false){
+        if((*head)->data == v){
+            addFirst(head, u);
+            return;
+        }
+        else{
+            struct node* temp = *head;
+            while((temp->next)->data != v){
+                temp = temp->next;
+            }
+
+            if(temp != NULL){
+                struct node* newNode = createNode(u);
+                newNode->next = temp->next;
+                temp->next = newNode;
+            }
+            return;
+        }
+    }
+}
+
+void addAfter(struct node** head, int u, int v){
+    struct node* temp = *head;
+    if(existNode(temp, u) == false){
+        while(temp->data != v){
+            temp = temp->next;
+        }
+
+        if(temp != NULL){
+            struct node* newNode = createNode(u);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+
+        return;
+    }
+}
+
+void remove(struct node** head, int k){
+    if(existNode(*head, k) == true){
+        struct node* temp = *head;
+        if((*head)->data == k){
+            (*head) = (*head)->next;
+            free(temp);
+        }
+        else{
+            while(temp->next->data != k){
+                temp = temp->next;
+            }
+                    struct node* t = temp->next;
+            temp->next = t->next;
+            free(t);
+
+        }
+    }
+}
+
+void reverse(struct node** head){
+    struct node* prev = NULL;
+    struct node* curr = (*head);
+    struct node* next = (*head)->next;
+    if(next == NULL) return;
+    while(next != NULL){
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        next = curr->next;
+    }
+
+    curr->next = prev;
+    *head = curr;
+}
+
+void printLinkedList(struct node* head){
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
 int main(){
     int n;
     cin >> n;
-    
+    struct node* head = NULL;
+    int x;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        addLast(&head, x);
+    }
+
+    string s;
+
+    while(getline(cin, s)){
+        if(s == "#") {
+            printLinkedList(head);
+            break;
+        }
+        stringstream ss(s);
+        string word;
+        string a[3];
+        int i = 0;
+        while(ss >> word){
+            a[i] = word;
+            i ++;
+        }
+
+        int u, v;
+
+        if(a[0] == "addlast"){
+            u = stoi(a[1]);
+            addLast(&head, u);
+        }
+
+        else if(a[0] == "addfirst"){
+            u = stoi(a[1]);
+            addFirst(&head, u);
+        }
+
+        else if(a[0] == "addafter"){
+            u = stoi(a[1]);
+            v = stoi(a[2]);
+            addAfter(&head, u, v);
+        }
+        else if(a[0] == "addbefore"){
+            u = stoi(a[1]);
+            v = stoi(a[2]);
+            addBefore(&head, u, v);
+        }
+        else if(a[0] == "remove"){
+            u = stoi(a[1]);
+            remove(&head, u);
+        }
+        else if(a[0] == "reverse"){
+            reverse(&head);
+        }
+
+    }
+
+    return 0;
 }
--- P_LINKED_LIST_MANIPULATION/Main.java
+++ P_LINKED_LIST_MANIPULATION/Main.java
@@ -1,7 +1,80 @@
 package P_LINKED_LIST_MANIPULATION;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+        int n = scanner.nextInt();
+        for(int i = 0; i < n; i++){
+            list.add(scanner.nextInt());
+        }
+
+        String s;
+        String[] a = new String[3];
+        int u, v, i, j;
+        while(true){
+            s = scanner.nextLine();
+            if(s.equals("#") == true){
+                break;
+            }
+            
+            a = s.split(" ");
+
+            if(a[0].equals("addfirst") == true){
+                u = Integer.parseInt(a[1]);
+                if(list.contains(u) == false){
+                    list.addFirst(u);
+                }
+            }
+            else if(a[0].equals("addlast") == true){
+                u = Integer.parseInt(a[1]);
+                if(list.contains(u) == false){
+                    list.addLast(u);
+                }
+            }
+            else if(a[0].equals("addafter") == true){
+                u = Integer.parseInt(a[1]);
+                v = Integer.parseInt(a[2]);
+                i = list.indexOf(u);
+                j = list.indexOf(v);
+
+                if(i == -1 && j != -1){
+                    list.add(j + 1, u);
+                }
+            }
+            else if(a[0].equals("addbefore") == true){
+                u = Integer.parseInt(a[1]);
+                v = Integer.parseInt(a[2]);
+                i = list.indexOf(u);
+                j = list.indexOf(v);
+
+                if(i == -1 && j != -1){
+                    list.add(j, u);
+                }
+            }
+            else if(a[0].equals("remove") == true){
+                u = Integer.parseInt(a[1]);
+                i = list.indexOf(u);
+                if(i != -1){ 
+                    list.remove(i);
+                }
+            }
+            else if(a[0].equals("reverse") == true){
+                LinkedList<Integer> revLinkedList = new LinkedList<>();
+                for(int k = list.size() - 1; k >= 0; k--){
+                    revLinkedList.add(list.get(k));
+                }
+                list = revLinkedList;
+            }
+        }
+
+        for(int x : list){
+            System.out.print(x + " ");
+        }
+
+        scanner.close();
     }
 }
